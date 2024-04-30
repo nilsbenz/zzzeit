01cfb3cbee6f44ec5c4271c58e0d5e352f938618
nilsbenz <nils.benz@icloud.com>
2024-04-30 16:09:08 +0200

    chore: add firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7tqsoXMjqyHEg5NvaRbZwY9W0FYpalxI",
+  authDomain: "zzzeit.firebaseapp.com",
+  projectId: "zzzeit",
+  storageBucket: "zzzeit.appspot.com",
+  messagingSenderId: "263162817338",
+  appId: "1:263162817338:web:646bbb554f15a2c609fe03",
+};
+
+const firebase = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+const auth = getAuth(firebase);
+const db = getFirestore(firebase);
+
+export { auth, db };
+export default firebase;
